f0ccf8bfea98aa5733613869093c907a4097c8c7
guiyilin <yilin.gui@gmail.com>
2021-03-16 15:06:24 +0800

    add cmdline usage

--- livox_parser.py
+++ livox_parser.py
@@ -1,242 +1,268 @@
 import os
 import sys
-import numpy
+import numpy as np
 import struct
 import time
 
+from scipy.spatial.transform import Rotation as R
+
 def parse_version_1_1(fd, verbose = 0):
-	bytes_counter = 0
-	DATA = fd.read()
+    bytes_counter = 0
+    DATA = fd.read()
 
-	PRIVATE_HEADER_BLOCK = DATA[:5]
-	bytes_counter += 5
-	FRAME_DURATION, DEVICE_COUNT = struct.unpack("=IB", PRIVATE_HEADER_BLOCK)
+    PRIVATE_HEADER_BLOCK = DATA[:5]
+    bytes_counter += 5
+    FRAME_DURATION, DEVICE_COUNT = struct.unpack("=IB", PRIVATE_HEADER_BLOCK)
 
-	if verbose == 1:
-		print(f"FRAME_DURATION is {FRAME_DURATION}")
-		print(f"DEVICE COUNT is {DEVICE_COUNT}")
+    if verbose == 1:
+        print(f"FRAME_DURATION is {FRAME_DURATION}")
+        print(f"DEVICE COUNT is {DEVICE_COUNT}")
 
-	devices_info = []
-	for _ in range(DEVICE_COUNT):
-		device_info_block = DATA[bytes_counter:bytes_counter+59]
-		bytes_counter += 59
+    devices_info = []
+    for _ in range(DEVICE_COUNT):
+        device_info_block = DATA[bytes_counter:bytes_counter+59]
+        bytes_counter += 59
 
-		LIDAR_SN, HUB_SN, DEVICE_IDX, DEVICE_TYPE, EXTRINSIC_ENABLE, ROLL, PITCH, YAW, X, Y, Z = struct.unpack(
-			"=16s16sBBBffffff", device_info_block)
-		devices_info.append([LIDAR_SN, HUB_SN, DEVICE_IDX, DEVICE_TYPE, EXTRINSIC_ENABLE, ROLL, PITCH, YAW, X, Y, Z])
-		if verbose == 1:
-			print(_, LIDAR_SN, HUB_SN, DEVICE_IDX, DEVICE_TYPE, EXTRINSIC_ENABLE, ROLL, PITCH, YAW, X, Y, Z)
+        LIDAR_SN, HUB_SN, DEVICE_IDX, DEVICE_TYPE, EXTRINSIC_ENABLE, ROLL, PITCH, YAW, X, Y, Z = struct.unpack(
+            "=16s16sBBBffffff", device_info_block)
+        devices_info.append([LIDAR_SN, HUB_SN, DEVICE_IDX, DEVICE_TYPE, EXTRINSIC_ENABLE, ROLL, PITCH, YAW, X, Y, Z])
+        if verbose == 1:
+            print(_, LIDAR_SN, HUB_SN, DEVICE_IDX, DEVICE_TYPE, EXTRINSIC_ENABLE, ROLL, PITCH, YAW, X, Y, Z)
 
-	points = []
+    points = []
 
-	while True:
-		frame_header = DATA[bytes_counter:bytes_counter+24]
-		if len(frame_header) != 24:
-			break
-		current_offset, next_offset, frame_index = struct.unpack("<qqq",frame_header )
-		bytes_counter += 24
+    xyzi_file_idx = 0
+    rot_pitch = R.from_euler('y', 30, degrees=True)
+    while True:
+        frame_header = DATA[bytes_counter:bytes_counter+24]
+        if len(frame_header) != 24:
+            break
+        current_offset, next_offset, frame_index = struct.unpack("<qqq",frame_header )
+        bytes_counter += 24
 
-		frame_points = []
-		while True:
-			PACKAGE_BEGIN_BLOCK = DATA[bytes_counter:bytes_counter+19]
+        frame_points = []
+        while True:
+            PACKAGE_BEGIN_BLOCK = DATA[bytes_counter:bytes_counter+19]
 
-			if len(PACKAGE_BEGIN_BLOCK) != 19:
-				points.extend(frame_points)
-				break
-			device_idx, version, slot_id, lidar_id, reserved, status_code, timestamp_type, data_type, timestamp = struct.unpack(
-				"<BBBBBIBBQ", PACKAGE_BEGIN_BLOCK)
+            if len(PACKAGE_BEGIN_BLOCK) != 19:
+                points.extend(frame_points)
+                break
+            device_idx, version, slot_id, lidar_id, reserved, status_code, timestamp_type, data_type, timestamp = struct.unpack(
+                "<BBBBBIBBQ", PACKAGE_BEGIN_BLOCK)
 
-			if version != 5:
-				points.extend(frame_points)
-				break
+            if version != 5:
+                points.extend(frame_points)
+                break
 
-			bytes_counter += 19
+            bytes_counter += 19
+            #print(data_type)
 
-			if data_type == 0:
-				#read 100 points * 13
-				for _ in range(100):
-					point_block = DATA[bytes_counter: bytes_counter + 13]
-					if len(point_block) != 13:
-						points.extend(frame_points)
-						break
-					x,y,z,r = struct.unpack("<iiiB",point_block)
-					frame_points.append([x,y,z,r])
-					bytes_counter += 13
-			elif data_type == 1:
-				#read 100 points * 9
-				for _ in range(100):
-					point_block = DATA[bytes_counter: bytes_counter + 9]
-					if len(point_block) != 9:
-						points.extend(frame_points)
-						break
-					depth, theta, phi, r = struct.unpack("<iHHB", point_block)
-					frame_points.append([depth,theta,phi,r])
-					bytes_counter += 13
-			elif data_type == 2:
-				#read 96 points * 14
-				# point_block = fd.read(96 * 14)
-				for _ in range(96):
-					point_block = DATA[bytes_counter: bytes_counter + 14]
-					if len(point_block) != 14:
-						points.extend(frame_points)
-						break
-					x, y, z, r, tag = struct.unpack("<iiiBB", point_block)
-					frame_points.append([x,y,z,r,tag])
-					bytes_counter += 14
-			elif data_type == 3:
-				#read 96 points * 10
-				# point_block = fd.read(96 * 10)
-				for _ in range(96):
-					point_block = DATA[bytes_counter: bytes_counter + 10]
-					if len(point_block) != 10:
-						points.extend(frame_points)
-						break
-					depth, theta, phi, r, tag = struct.unpack("<iHHBB", point_block)
-					frame_points.append([depth, theta, phi, r, tag])
-					bytes_counter += 10
-			elif data_type == 4:
-				#read 48 points * 28
-				# point_block = fd.read(48 * 28)
-				for _ in range(48):
-					point_block = DATA[bytes_counter: bytes_counter + 28]
-					if len(point_block) != 28:
-						points.extend(frame_points)
-						break
-					x1, y1, z1, r1,tag1,x2,y2,z2,r2,tag2 = struct.unpack("<iiiBBiiiBB", point_block)
-					frame_points.append([x1, y1, z1, r1,tag1,x2,y2,z2,r2,tag2])
-					bytes_counter += 28
-			elif data_type == 5:
-				#read 48 points * 24
-				# point_block = fd.read(48 * 24)
-				for _ in range(48):
-					point_block = DATA[bytes_counter: bytes_counter + 24]
-					if len(point_block) != 24:
-						points.append(frame_points)
-						break
-					theta, phi, depth1, r, tag1, depth2, r2, tag2, gyro_x = struct.unpack("<iiiBBiBBf", point_block)
-					frame_points.append([theta, phi, depth1, r, tag1, depth2, r2, tag2, gyro_x])
-					bytes_counter += 24
-			elif data_type == 6:
-				#read 1 point * 48
-				point_block = DATA[bytes_counter: bytes_counter + 48]
-				if len(point_block) != 48:
-					points.extend(frame_points)
-					break
-				gyro_x, gyro_y, gyro_z, acc_x,acc_y,acc_z = struct.unpack("<ffffff", point_block)
-				frame_points.append([gyro_x, gyro_y, gyro_z, acc_x,acc_y,acc_z])
-				bytes_counter += 48
-			else:
-				print("unknown data type", data_type)
+            if data_type == 0:
+                #read 100 points * 13
+                for _ in range(100):
+                    point_block = DATA[bytes_counter: bytes_counter + 13]
+                    if len(point_block) != 13:
+                        points.extend(frame_points)
+                        break
+                    x,y,z,r = struct.unpack("<iiiB",point_block)
+                    frame_points.append([x,y,z,r])
+                    bytes_counter += 13
+            elif data_type == 1:
+                #read 100 points * 9
+                for _ in range(100):
+                    point_block = DATA[bytes_counter: bytes_counter + 9]
+                    if len(point_block) != 9:
+                        points.extend(frame_points)
+                        break
+                    depth, theta, phi, r = struct.unpack("<iHHB", point_block)
+                    frame_points.append([depth,theta,phi,r])
+                    bytes_counter += 13
+            elif data_type == 2:
+                #read 96 points * 14
+                # point_block = fd.read(96 * 14)
+                for _ in range(96):
+                    point_block = DATA[bytes_counter: bytes_counter + 14]
+                    if len(point_block) != 14:
+                        points.extend(frame_points)
+                        break
+                    x, y, z, r, tag = struct.unpack("<iiiBB", point_block)
+                    frame_points.append([x,y,z,r,tag])
+                    bytes_counter += 14
+            elif data_type == 3:
+                #read 96 points * 10
+                # point_block = fd.read(96 * 10)
+                for _ in range(96):
+                    point_block = DATA[bytes_counter: bytes_counter + 10]
+                    if len(point_block) != 10:
+                        points.extend(frame_points)
+                        break
+                    depth, theta, phi, r, tag = struct.unpack("<iHHBB", point_block)
+                    frame_points.append([depth, theta, phi, r, tag])
+                    bytes_counter += 10
+            elif data_type == 4:
+                #read 48 points * 28
+                # point_block = fd.read(48 * 28)
+                for _ in range(48):
+                    point_block = DATA[bytes_counter: bytes_counter + 28]
+                    if len(point_block) != 28:
+                        points.extend(frame_points)
+                        break
+                    x1, y1, z1, r1,tag1,x2,y2,z2,r2,tag2 = struct.unpack("<iiiBBiiiBB", point_block)
+                    frame_points.append([x1, y1, z1, r1,tag1,x2,y2,z2,r2,tag2])
+                    bytes_counter += 28
+            elif data_type == 5:
+                #read 48 points * 24
+                # point_block = fd.read(48 * 24)
+                for _ in range(48):
+                    point_block = DATA[bytes_counter: bytes_counter + 24]
+                    if len(point_block) != 24:
+                        points.append(frame_points)
+                        break
+                    theta, phi, depth1, r, tag1, depth2, r2, tag2, gyro_x = struct.unpack("<iiiBBiBBf", point_block)
+                    frame_points.append([theta, phi, depth1, r, tag1, depth2, r2, tag2, gyro_x])
+                    bytes_counter += 24
+            elif data_type == 6:
+                #read 1 point * 48
+                point_block = DATA[bytes_counter: bytes_counter + 48]
+                if len(point_block) != 48:
+                    points.extend(frame_points)
+                    break
+                gyro_x, gyro_y, gyro_z, acc_x,acc_y,acc_z = struct.unpack("<ffffff", point_block)
+                frame_points.append([gyro_x, gyro_y, gyro_z, acc_x,acc_y,acc_z])
+                bytes_counter += 48
+            else:
+                print("unknown data type", data_type)
+        # print(len(frame_points))
+        # print(frame_points)
+        xyzit_points = np.array([[pts[0], pts[1], pts[2], pts[3], pts[4]] for pts in frame_points])
+        xyzi_points = np.array([[pts[0], pts[1], pts[2], pts[3]] for pts in frame_points])
+        xyz_points = np.array([rot_pitch.apply([pts[0], pts[1], pts[2]]) for pts in frame_points])
+        global OUT_DIR
+        # print(xyzi_points)
+        # print(rot_pitch.apply([1, 1, 1]))
+        #np.savetxt('{:0>4d}.xyz'.format(xyzi_file_idx), xyzi_points)
+        #np.savetxt('{:0>4d}_rot.xyz'.format(xyzi_file_idx), xyz_points)
+        #np.savetxt('{}/{:0>4d}.xyz'.format(OUT_DIR, xyzi_file_idx), xyzit_points)
+        out_fname = '{}/{:0>4d}.xyz'.format(OUT_DIR, xyzi_file_idx)
+        with open(out_fname, 'w') as f_out_xyz:
+            for pt in frame_points:
+                f_out_xyz.write('{} {} {} {} {}\n'.format(pt[0], pt[1], pt[2], pt[3], pt[4]))
 
-	fd.close()
+        xyzi_file_idx += 1
+    fd.close()
 
-	return points
+    if verbose:
+        print(f"number of points: {len(points)}")
+    return points
 
 def parse_version_1_0(fd, verbose = 0):
-	PRIVATE_HEADER_BLOCK = fd.read(1)
-	DEVICE_COUNT = struct.unpack("=B", PRIVATE_HEADER_BLOCK)[0]
+    PRIVATE_HEADER_BLOCK = fd.read(1)
+    DEVICE_COUNT = struct.unpack("=B", PRIVATE_HEADER_BLOCK)[0]
 
-	if verbose:
-		print("DEVICE_COUNT:", DEVICE_COUNT)
+    if verbose:
+        print("DEVICE_COUNT:", DEVICE_COUNT)
 
-	DEVICES_INFO = []
+    DEVICES_INFO = []
 
-	for device_idx in range(DEVICE_COUNT):
-		DEVICE_INFO_BLOCK = fd.read(58)
+    for device_idx in range(DEVICE_COUNT):
+        DEVICE_INFO_BLOCK = fd.read(58)
 
-		LIDAR_SN, HUB_SN, DEVICE_IDX, DEVICE_TYPE, ROLL, PITCH, YAW, X, Y, Z = struct.unpack("=16s16sBBffffff",
-																							 DEVICE_INFO_BLOCK)
+        LIDAR_SN, HUB_SN, DEVICE_IDX, DEVICE_TYPE, ROLL, PITCH, YAW, X, Y, Z = struct.unpack("=16s16sBBffffff", DEVICE_INFO_BLOCK)
 
-		# print("LIDAR_SN ", LIDAR_SN)
-		# print("HUB_SN ", HUB_SN)
-		# print("DEVICE_IDX ", DEVICE_IDX)
-		# print("DEVICE TYPE ", DEVICE_TYPE)
-		# print("ROLL ", ROLL)
-		# print("PITCH ", PITCH)
-		# print("YAW ", YAW)
-		# print("X ", X)
-		# print("Y ", Y)
-		# print("Z ", Z)
+        # print("LIDAR_SN ", LIDAR_SN)
+        # print("HUB_SN ", HUB_SN)
+        # print("DEVICE_IDX ", DEVICE_IDX)
+        # print("DEVICE TYPE ", DEVICE_TYPE)
+        # print("ROLL ", ROLL)
+        # print("PITCH ", PITCH)
+        # print("YAW ", YAW)
+        # print("X ", X)
+        # print("Y ", Y)
+        # print("Z ", Z)
 
-		DEVICES_INFO.append([LIDAR_SN, HUB_SN, DEVICE_IDX, ROLL, PITCH, YAW, X, Y, Z])
+        DEVICES_INFO.append([LIDAR_SN, HUB_SN, DEVICE_IDX, ROLL, PITCH, YAW, X, Y, Z])
 
-	FRAMES = {}
-	current_timestamp = None
+    FRAMES = {}
+    current_timestamp = None
 
-	POINTS = []
+    POINTS = []
 
-	start_time = time.time()
+    start_time = time.time()
 
-	while True:
-		FRAME_HEADER_BLOCK = fd.read(32)
+    while True:
+        FRAME_HEADER_BLOCK = fd.read(32)
 
-		if FRAME_HEADER_BLOCK == b'':
-			if verbose:
-				print("end of file")
-			break
+        if FRAME_HEADER_BLOCK == b'':
+            if verbose:
+                print("end of file")
+            break
 
-		Current_offset, Next_offset, Frame_index, Package_count = struct.unpack("<qqqq", FRAME_HEADER_BLOCK)
-		if verbose:
-			print("Frame header info:", Current_offset, Next_offset, Frame_index, Package_count)
+        Current_offset, Next_offset, Frame_index, Package_count = struct.unpack("<qqqq", FRAME_HEADER_BLOCK)
+        if verbose:
+            print("Frame header info:", Current_offset, Next_offset, Frame_index, Package_count)
 
-		for package_idx in range(Package_count):
-			Package_block = fd.read(1319)
+        for package_idx in range(Package_count):
+            Package_block = fd.read(1319)
 
-			device_idx, Version, Slot_id, LIDAR_id, Reserved, Status_code, Timestamp_type, Data_type, Timestamp = struct.unpack(
-				"=BBBBBIBBQ", Package_block[:19])
+            device_idx, Version, Slot_id, LIDAR_id, Reserved, Status_code, Timestamp_type, Data_type, Timestamp = struct.unpack(
+                "=BBBBBIBBQ", Package_block[:19])
 
-			if Timestamp != current_timestamp:
-				if current_timestamp:
-					FRAMES[current_timestamp] = POINTS
-					POINTS.clear()
+            if Timestamp != current_timestamp:
+                if current_timestamp:
+                    FRAMES[current_timestamp] = POINTS
+                    POINTS.clear()
 
-					if verbose:
-						print(f"Считывание одного таймштампа заняло {time.time() - start_time}")
-					start_time = time.time()
+                    if verbose:
+                        print(f"Считывание одного таймштампа заняло {time.time() - start_time}")
+                    start_time = time.time()
 
-				current_timestamp = Timestamp
+                current_timestamp = Timestamp
 
-			points_block = Package_block[19:]
-			for idx in range(100):
-				point = struct.unpack("<fffB", points_block[idx * 13:(idx + 1) * 13])
-				POINTS.append(point)
-	if verbose:
-		print(f"Всего насчитано: {len(POINTS)} points")
+            points_block = Package_block[19:]
+            for idx in range(100):
+                point = struct.unpack("<fffB", points_block[idx * 13:(idx + 1) * 13])
+                POINTS.append(point)
+    if verbose:
+        print(f"Всего насчитано: {len(POINTS)} points")
 
-	fd.close()
+    fd.close()
 
 # FILE_PATH = 'Livox LiDAR - Mid-100 Point Cloud Data %231.lvx'
 FILE_PATH = '2020-09-09_17-12-51.lvx'
 
 if __name__ == "__main__":
+    global OUT_DIR
+    if len(sys.argv) != 3:
+        print('Usage: {} <lvx_filepath> <out_dir>'.format(sys.argv[0]))
+        sys.exit(1)
+    #if len(sys.argv) == 2:
+    #    FILE_PATH = sys.argv[1]
+    FILE_PATH = sys.argv[1]
+    OUT_DIR = sys.argv[2]
+    if not os.path.exists(OUT_DIR):
+        os.system('mkdir -p {}'.format(OUT_DIR))
+    fd = open(FILE_PATH,"rb")
+    # data = fd.read()
 
-	fd = open(FILE_PATH,"rb")
-	# data = fd.read()
+    PUBLIC_HEADER_DATA =fd.read(24)
+    PUBLIC_HEADER, VERSION_A, VERSION_B, VERSION_C, VERSION_D, MAGIC_CODE = struct.unpack("=16sccccI",PUBLIC_HEADER_DATA)
+    # print(PUBLIC_HEADER)
+    # print(hex(MAGIC_CODE))
 
-	PUBLIC_HEADER_DATA =fd.read(24)
-	PUBLIC_HEADER, VERSION_A, VERSION_B, VERSION_C, VERSION_D, MAGIC_CODE = struct.unpack("=16sccccI",PUBLIC_HEADER_DATA)
-	# print(PUBLIC_HEADER)
-	# print(hex(MAGIC_CODE))
+    VERSION_A = int.from_bytes(VERSION_A, "little")
+    VERSION_B = int.from_bytes(VERSION_B, "little")
+    VERSION_C = int.from_bytes(VERSION_C,"little")
+    VERSION_D = int.from_bytes(VERSION_D,"little")
 
-	VERSION_A = int.from_bytes(VERSION_A, "little")
-	VERSION_B = int.from_bytes(VERSION_B, "little")
-	VERSION_C = int.from_bytes(VERSION_C,"little")
-	VERSION_D = int.from_bytes(VERSION_D,"little")
+    print(f"version {VERSION_A}_{VERSION_B}_{VERSION_C}_{VERSION_D}")
 
-	print(f"version {VERSION_A}_{VERSION_B}_{VERSION_C}_{VERSION_D}")
+    assert VERSION_A == 1, "VERSION_A"
+    assert VERSION_B in [0,1], "VERSION_B"
+    assert VERSION_C == 0, "VERSION_C"
+    assert VERSION_D == 0, "VERSION_D"
+    assert hex(MAGIC_CODE) == "0xAC0EA767".lower(), "MAGIC_CODE"
 
-	assert VERSION_A == 1, "VERSION_A"
-	assert VERSION_B in [0,1], "VERSION_B"
-	assert VERSION_C == 0, "VERSION_C"
-	assert VERSION_D == 0, "VERSION_D"
-	assert hex(MAGIC_CODE) == "0xAC0EA767".lower(), "MAGIC_CODE"
-
-	if VERSION_A == 1 and VERSION_B == 0:
-		parse_version_1_0(fd)
-	elif VERSION_A == 1 and VERSION_B == 1:
-		parse_version_1_1(fd)
-
-
-
-
-
+    if VERSION_A == 1 and VERSION_B == 0:
+        parse_version_1_0(fd)
+    elif VERSION_A == 1 and VERSION_B == 1:
+        parse_version_1_1(fd, 1)
